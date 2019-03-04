6b9350f7ddbd20c0ee2c63ba051baa3c703318af
Uzair Iqbal <uzair.qq@outlook.com>
2019-03-05 03:51:36 +0500

    Few Migrations

--- MilkManagement.Domain/Migrations/20190304144803_AddedRatesInCustomerSupplied.Designer.cs
+++ MilkManagement.Domain/Migrations/20190304144803_AddedRatesInCustomerSupplied.Designer.cs
@@ -2,6 +2,7 @@
 using System;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 using MilkManagement.Domain;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
@@ -9,9 +10,10 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace MilkManagement.Domain.Migrations
 {
     [DbContext(typeof(MilkManagementDbContext))]
-    partial class MilkManagementDbContextModelSnapshot : ModelSnapshot
+    [Migration("20190304144803_AddedRatesInCustomerSupplied")]
+    partial class AddedRatesInCustomerSupplied
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ MilkManagement.Domain/Migrations/20190304144803_AddedRatesInCustomerSupplied.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MilkManagement.Domain.Migrations
+{
+    public partial class AddedRatesInCustomerSupplied : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<int>(
+                name: "Rate",
+                table: "CustomerSupplied",
+                nullable: false,
+                defaultValue: 0);
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Rate",
+                table: "CustomerSupplied");
+        }
+    }
+}
