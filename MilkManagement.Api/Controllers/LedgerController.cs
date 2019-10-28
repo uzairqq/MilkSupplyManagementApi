f2b5d733b3cd2939867fadc7a755b2bdd09905cc
Muhammad uzair <uzairiqbal@Muhammads-MacBook-Pro.local>
2019-10-28 16:46:52 +0500

    ledger changes

--- MilkManagement.Api/Controllers/LedgerController.cs
+++ MilkManagement.Api/Controllers/LedgerController.cs
@@ -6,90 +6,27 @@ using Microsoft.Extensions.Logging;
 using MilkManagement.Domain.Dto;
 using MilkManagement.Services.Services.Interfaces;
 
-namespace MilkManagement.Api.Controllers
-{
+namespace MilkManagement.Api.Controllers {
 
-    [Route("api/[controller]")]
-    public class LedgerController : Controller
-    {
+    [Route ("api/[controller]")]
+    public class LedgerController : Controller {
         private readonly ILedgerService _ledgerService;
         private readonly ILogger<LedgerController> _logger;
 
-        public LedgerController(ILogger<LedgerController> logger,
-            ILedgerService ledgerService)
-        {
+        public LedgerController (ILogger<LedgerController> logger,
+            ILedgerService ledgerService) {
             _logger = logger;
             _ledgerService = ledgerService;
         }
 
-        [HttpGet]
-        public IActionResult Get()
-        {
-            try
-            {
-                return Ok();
-            }
-            catch (Exception)
-            {
-                _logger.LogError("Failed to execute GET");
-                return BadRequest();
-            }
-        }
-
-        [HttpGet("selectedDate/{date}")]
-        public async Task<IActionResult> Get([FromRoute] DateTime date)
-        {
-            try
-            {
-                var result = await _ledgerService.Get(date);
-                return Ok(result);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex.ToString());
-                return BadRequest();
-            }
-        }
-
-        [HttpPost]
-        public IActionResult Post([FromBody] LedgerDto model)
-        {
-            try
-            {
-                return Created("", null);
-            }
-            catch (Exception)
-            {
-                _logger.LogError("Failed to execute POST");
-                return BadRequest();
-            }
-        }
-
-        [HttpPut]
-        public IActionResult Put([FromBody] LedgerDto model)
-        {
-            try
-            {
-                return Ok();
-            }
-            catch (Exception)
-            {
-                _logger.LogError("Failed to execute PUT");
-                return BadRequest();
-            }
-        }
-
-        [HttpDelete]
-        public IActionResult Delete(LedgerDto id)
-        {
-            try
-            {
-                return Ok();
-            }
-            catch (Exception)
-            {
-                _logger.LogError("Failed to execute DELETE");
-                return BadRequest();
+        [HttpGet ("selectedDate/{date}")]
+        public async Task<IActionResult> Get ([FromRoute] DateTime date) {
+            try {
+                var result = await _ledgerService.Get (date);
+                return Ok (result);
+            } catch (Exception ex) {
+                _logger.LogError (ex.ToString ());
+                return BadRequest ();
             }
         }
     }
